dd4703e13cff65287e02a934801d05e2c536600b
Mac <mac@Macs-MacBook-Pro.local>
2022-01-09 11:34:01 -0800

    commit to main

--- graph_test.py
+++ graph_test.py
@@ -1,15 +1,17 @@
 from bokeh.plotting import figure, show
 import requests
+import pandas as pd
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo'
 r = requests.get(url)
 data = r.json()
-print(data)
+
+df = pd.read_json(data)
 
 # prepare some data
-x = [1, 2, 3, 4, 5]
-y = [6, -4, 2, 4, 5]
+x = [i for i in len(df['1. open'])]
+y = df['1. open']
 
 # create a new plot with a title and axis labels
 p = figure(title="Simple line example", x_axis_label='x', y_axis_label='y')
