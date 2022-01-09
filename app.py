ba5a5917aa00f3de4aff5de1f555316eaab61cc4
Mac <mac@Macs-MacBook-Pro.local>
2022-01-09 11:27:14 -0800

    commit to main

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request, redirect
+import requests
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo'
+r = requests.get(url)
+data = r.json()
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index', methods=['POST', 'GET'])
 def index():
-  return render_template('index.html')
+  if request.method == 'GET':
+    return render_template('index.html')
+  if request.method == 'POST':
+    print("Hellos")
+    print(request.form)
+    return render_template('about.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def no_route():
+  return index()
 
 @app.route('/about')
 def about():
